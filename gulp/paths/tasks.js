9025ab6a1a04d14cb0d5bbbee5900cee32dd23d4
Sergey <a@site.ru>
2018-01-20 00:16:46 +0300

    first commit

---
+++ gulp/paths/tasks.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = [
+	'./gulp/tasks/less.js',
+	'./gulp/tasks/html.js',
+	'./gulp/tasks/clean.js',
+	'./gulp/tasks/watch.js',
+	'./gulp/tasks/serv.js'
+
+	
+];
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+global.$ = {
+	path: {
+		task: require('./gulp/paths/tasks.js')
+	},
+	 gulp :require('gulp'),
+	 del: require('del'),
+	 gp : require("gulp-load-plugins")(),
+	 browserSync: require('browser-sync').create()
+};
+
+
+$.path.task.forEach(function(taskPath){
+	require(taskPath)();
+});
+
+$.gulp.task('default',$.gulp.series(
+	'clean','less','html',
+	$.gulp.parallel('watch','serv')
+	)
+	);
